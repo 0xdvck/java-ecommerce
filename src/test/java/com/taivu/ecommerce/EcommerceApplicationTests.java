cc4ca8f21ac0eae19062774f48c8946d1c57cec8
root <root@DESKTOP-D5QC3JQ.>
2024-12-18 15:41:15 +0700

    Init project

---
+++ src/test/java/com/taivu/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taivu.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
